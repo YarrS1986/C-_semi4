2edf69a73312374cb8eb0fad66fb123ba2068ef3
Yaroslav.S <sysoev.yar@yandex.ru>
2022-04-09 17:52:09 +0500

    Зачада о заполнении массива из 8 числе от 0 до 1

---
+++ example3_arrayof8elements/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// Пример, [1,0,1,1,0,1,0,0]
+
+Console.Clear();
+
+int[] array = new int[8];
+
+for (int ind = 0; ind < 8; ind++)
+{
+    Random rnd = new Random();
+    int rndValue = rnd.Next(0, 2);
+    array[ind] = rndValue;
+    Console.Write($"{array[ind]} ");
+}
