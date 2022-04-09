5334ae8b0985243903f82c4fca049e36ff100919
Yaroslav.S <sysoev.yar@yandex.ru>
2022-04-09 20:20:21 +0500

    ДЗ.2 разложить число и суммирвать цифры

---
+++ homework2_sumvalue/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// Пример, 452 -> 11, 82 -> 10, 9012 -> 12
+Console.Clear();
+
+int value = Promt("Введи число = ");
+int sum = 0;
+
+if (value >= 0)
+{
+    while (value != 0) // Раскладываем число по цифрам и суммируем
+    {
+        int num = value % 10;
+        value = value / 10;
+        sum = sum + num;
+
+    }
+    Console.WriteLine($"Число = {sum}");
+}
+else Console.WriteLine($"Введи положительное, целое число!");
+
+Console.WriteLine();
+
+
+//-----------------------------------------------------------------
+int Promt(string message)
+{
+    Console.Write(message);
+    string strMessage = Console.ReadLine();
+    int answer = int.Parse(strMessage);
+    return answer;
+}
