f32fadd38cfa7db60cd5bea653476cb7eeb033a3
Yaroslav.S <sysoev.yar@yandex.ru>
2022-04-09 16:03:24 +0500

    Зачада о сумме всех элементов в массиве

--- example1_numberA/Program.cs
+++ example1_numberA/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// пример, 7 -> 28, 4 -> 10, 8 -> 36
+
+Console.Clear();
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string strMessage = Console.ReadLine();
+    int answer = int.Parse(strMessage);
+    return answer;
+}
+
+void SumArray(int[] array) // этот метод задает массив
+{
+    int count = array.Length;
+    int sum = 0;
+    for (int ind = 1; ind <= count; ind++)
+    {
+        sum = sum + ind;
+    }
+    Console.Write($"{sum} ");
+}
+
+int value = Promt("Введи длину массива = ");
+int[] arr = new int[value];
+SumArray(arr);
