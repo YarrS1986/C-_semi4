92ef02e0224ef48b910fd139228b4cb2ca8b54b8
Yaroslav.S <sysoev.yar@yandex.ru>
2022-04-09 21:17:47 +0500

    ДЗ.3 заполнить массив через функции

---
+++ homework3_arrayofrandnum/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран. 
+// Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах).
+// Пример 1,2,5,7,19,1,2,8 -> 1,2,5,7,19,1,2,8                6,1,33,5,6,7,7,1 -> 6,1,33,5,6,7,7,1
+
+Console.Clear();
+
+int[] arr = new int[8];
+
+FindArray(arr);
+FillArray(arr);
+
+//--------------------------------------------
+void FindArray(int[] array)
+{
+    for (int index = 0; index < array.Length; index++)
+    {
+        array[index] = new Random().Next(1, 10);
+    }
+}
+
+void FillArray(int[] collection)
+{
+    Console.Write($"[ ");
+    for (int index = 0; index < collection.Length - 1; index++)
+    {
+        Console.Write($"{collection[index]}, ");
+    }
+    Console.Write($"{collection[collection.Length - 1]} ]");
+}
