9aa2d0e5c7f4e01a45f7a36ec6a3c1fb53295929
Yaroslav.S <sysoev.yar@yandex.ru>
2022-04-09 19:26:51 +0500

    ДЗ число А в степень В

---
+++ homework1_a^b/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Пример,              3, 5 -> 243 (3⁵),                      2, 4 -> 16
+
+Console.Clear();
+
+
+int A = Promt("Введи число А = ");
+int B = Promt("Введи число B = ");
+int sum = 1;
+
+for (int stepen = 1; stepen <= B; stepen++)
+{
+    sum *= A;
+}
+
+Console.WriteLine();
+Console.WriteLine($"Число {A} в степени {B} = {sum}");
+Console.WriteLine();
+
+
+//-----------------------------------------------------------------
+int Promt(string message)
+{
+    Console.Write(message);
+    string strMessage = Console.ReadLine();
+    int answer = int.Parse(strMessage);
+    return answer;
+}
